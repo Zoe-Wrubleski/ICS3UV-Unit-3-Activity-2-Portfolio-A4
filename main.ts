15776e1642bfddb22f1792996233feed2770f7c7
default <zoe.wrubleski@stu.ocsb.ca>
2025-11-20 14:22:19 +0000

    opposite order sentence display

---
+++ main.ts
@@ -0,0 +1,26 @@
+/**
+ * @author Zoe Wrubleski
+ * @version 1.0.0
+ * @date 2025-11-20
+ * @fileoverview This program displays sentences in opposite order than input
+ */
+
+// set variables
+let line1: string;
+let line2: string;
+let line3: string;
+let line4: string;
+
+// prompt user
+line1 = prompt("Enter your first sentence: ");
+line2 = prompt("Enter your second sentence: ");
+line3 = prompt("Enter your third sentence: ");
+line4 = prompt("Enter your fourth sentence: ");
+
+// display sentences 
+console.log(line4);
+console.log(line3)
+console.log(line2);
+console.log(line1);
+
+console.log("\nDone.")
